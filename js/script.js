a0cb4c62310fe59b86d97b22788fbf00bcd1dc53
itsmeyuki <128566598+itsmeyuukii@users.noreply.github.com>
2024-09-05 16:04:57 +0800

    Updated portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//  Text Animation Script
+var typed = new Typed(".typing",{
+    strings:["Web Designer","","Web Developer","Graphic Designer", "Back-End Developer", "Front-End Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
